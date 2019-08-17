38e36b4128b77c704caba63b4e9d6c5bf73b9f65
Edglatus <tabrinath@gmail.com>
2019-08-17 04:11:00 -0300

    Merged ex11 with ex12

--- exercicio11/js.js
+++ exercicio11/js.js
@@ -35,3 +35,21 @@ function callPromptv2()
 
 	alert(years + yearString + months + monthString + days + dayString);
 }
+
+function validateEmail()
+{
+	var input = prompt("Digite um E-Mail:");
+
+	var validation = input.split("@");
+
+	var dotValidation = validation[1].split(".");
+
+	if(validation.length != 2 || validation[0].length < 1 || dotValidation.length != 2 || dotValidation[0].length < 2)
+	{
+		alert("E-mail Inválido.");
+	}
+	else
+	{
+		alert("E-mail Válido");
+	}
+}
